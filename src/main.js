bab3c7169e1c93ed3adac46df28343ca85612ea4
Laveesh <gupta.laveesh@gmail.com>
2020-02-20 18:20:14 +0530

    working login and authentication

--- src/main.js
+++ src/main.js
@@ -1,13 +1,41 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import firebase from "firebase/app";
+import axios from "axios";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$axios = axios;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC36dbzd9bk16WzgXCcO5opT8mdvk4HTdE",
+  authDomain: "myapp-642e6.firebaseapp.com",
+  databaseURL: "https://myapp-642e6.firebaseio.com",
+  projectId: "myapp-642e6",
+  storageBucket: "myapp-642e6.appspot.com",
+  messagingSenderId: "398228149616",
+  appId: "1:398228149616:web:35889667efc0eb6e4d6bb5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let app;
+
+firebase.auth().onAuthStateChanged(user => {
+  console.log("user", user);
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+});
+
+// app = new Vue({
+//   router,
+//   store,
+//   render: h => h(App)
+// }).$mount("#app");
